34e11310766f6d9298cb30b67e5a9bc616c25cef
Jeremiah Olufayo <olufayojeremiah@gmail.com>
2018-10-16 10:23:16 +0100

    protect routes for signed in and signed out users

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import { createProject } from '../../store/actions/projectActions';
 
 class CreateProject extends Component {
@@ -16,11 +17,13 @@ class CreateProject extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    // console.log(this.state);
     this.props.createProject(this.state);
   }
 
   render() {
+    const { auth } = this.props;
+    if (!auth.uid) return <Redirect to='/signin' />
+
     return (
       <div className='container'>
         <form className="white" onSubmit={this.handleSubmit}>
@@ -42,10 +45,16 @@ class CreateProject extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth
+  }
+}
+
 const mapDispatchToProps = dispatch => {
   return {
     createProject: (project) => dispatch(createProject(project))
   }
 }
 
-export default connect(null, mapDispatchToProps) (CreateProject)
+export default connect(mapStateToProps, mapDispatchToProps) (CreateProject)
